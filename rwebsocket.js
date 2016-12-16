50711b159a18b46491e8c12d1a932fffe97b22d0
Maxime Tricoire <max.tricoire@gmail.com>
2016-12-16 16:54:55 +0100

    first commit

---
+++ rwebsocket.js
@@ -0,0 +1,75 @@
+var WebSocket = require('ws');
+
+function RWebSocket(url, protocols, retryInterval) {
+	this.url = url;
+	this.protocols = protocols;
+	this.retryInterval = retryInterval || 3000;
+}
+
+RWebSocket.prototype = {
+	connect: function () {
+		var self = this;
+		var connectTimeout;
+
+		this.client = new WebSocket(this.url, this.protocols);
+		this.readyState = this.client.readyState;
+		this.protocol = this.client.protocol;
+
+		this.client.onopen = function () {
+			clearTimeout(connectTimeout);
+			connectTimeout = null;
+			self.onopen();
+		};
+
+		this.client.onmessage = function (event) {
+			self.onmessage(event);
+		};
+
+		this.client.onclose = function (event) {
+			switch (event.code) {
+				case 1000:
+					// normal close
+					break;
+
+				default:
+					// abnormal close
+					if (!connectTimeout) {
+						connectTimeout = setTimeout(function () {
+							self.connect();
+						}, self.retryInterval);
+					}
+			}
+			self.onclose(event);
+		};
+
+		this.client.onerror = function (event) {
+			switch (event.code) {
+				case 'ECONNREFUSED':
+					if (!connectTimeout) {
+						connectTimeout = setTimeout(function () {
+							self.connect();
+						}, self.retryInterval);
+					}
+					break;
+
+				default:
+					self.onerror(event);
+					break;
+			}
+			self.onerror(event);
+		};
+	},
+
+	close: function (code, reason) {
+		if (this.client) {
+			this.client.close(code || 1000, reason);
+		}
+	},
+
+	onopen: function () {},
+	onmessage: function () {},
+	onclose: function () {},
+	onerror: function () {},
+};
+
+module.exports = RWebSocket;
---
+++ test/main.js
@@ -0,0 +1,57 @@
+var WebSocket = require('ws');
+
+var RWebSocket = require('../rwebsocket');
+
+var PORT = 9000;
+var URL = 'ws://localhost:' + PORT;
+
+describe('RWebSocket tests', function () {
+	this.timeout(3000);
+	this.slow(500);
+
+	var server;
+
+	function cleanServerClose(done) {
+		server.clients.forEach(function (client) {
+			client.close();
+		});
+		server.close(done);
+	}
+
+	beforeEach('init WebSocket server', function (done) {
+		server = new WebSocket.Server({ port: PORT }, done);
+	});
+
+	afterEach('close WebSocket server', function (done) {
+		cleanServerClose(done);
+	});
+
+	it('should connect when server is up', function (done) {
+		var client = new RWebSocket(URL);
+		client.connect();
+		client.onopen = function () {
+			client.close();
+			done();
+		};
+	});
+
+	it('should try to reconnect if server is down', function (done) {
+		this.slow(1000);
+
+		// close server properly
+		cleanServerClose(function () {
+			// re-up server after some time
+			setTimeout(function () {
+				server = new WebSocket.Server({ port: PORT });
+			}, 200);
+		});
+
+		// try to connect to closed server
+		var client = new RWebSocket(URL, null, 100);
+		client.connect();
+		client.onopen = function () {
+			client.close();
+			done();
+		};
+	});
+});
